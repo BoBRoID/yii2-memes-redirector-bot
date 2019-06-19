8afa6f05b330a2cc853acc7f25e25a1d4d869bdf
BoBRoID <n.gilko@gmail.com>
2019-06-19 15:11:39 +0300

    debug

--- src/actions/GetWebHookAction.php
+++ src/actions/GetWebHookAction.php
@@ -20,9 +20,7 @@ class GetWebHookAction extends Action
         try{
             $telegram = new Telegram(\Yii::$app->params['apiKey'], \Yii::$app->params['botName']);
 
-            \Yii::debug(\Yii::getAlias('@vendor/bobroid/memesRedirectorBot/src/commands/src'));
-
-            $telegram->addCommandsPath(\Yii::getAlias('@vendor/bobroid/memesRedirectorBot/src/commands/src'));
+            $telegram->addCommandsPath(\Yii::getAlias('@vendor/bobroid/yii2-memes-redirector-bot/src/commands/src'));
             $telegram->enableAdmins(Configuration::getAdminsIDs());
             $telegram->handle();
         }catch (TelegramException $e){
